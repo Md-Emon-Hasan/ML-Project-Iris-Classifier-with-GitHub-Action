f757a5170bf19ec2e0e20ead2706bb3dc5f06597
Md Emon Hasan <iconicemon01@gmail.com>
2024-08-27 12:44:53 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the pre-trained model
+model = pickle.load(open('model/iris_model.pkl', 'rb'))
+
+def predict_species(sepal_length, sepal_width, petal_length, petal_width):
+    features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    prediction = model.predict(features)
+    return prediction[0]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    sepal_length = float(request.form['sepal_length'])
+    sepal_width = float(request.form['sepal_width'])
+    petal_length = float(request.form['petal_length'])
+    petal_width = float(request.form['petal_width'])
+    
+    prediction = predict_species(sepal_length, sepal_width, petal_length, petal_width)
+    
+    return render_template('result.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ jupyter/model.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+# Load data
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# Train model
+model = RandomForestClassifier(n_estimators=100)
+model.fit(X_train, y_train)
+
+# Save model
+with open('model/iris_model.pkl', 'wb') as file:
+    pickle.dump(model, file)
